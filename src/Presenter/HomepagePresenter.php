3738634438a120eeec6bf423a65e3b47d54f0b62
Tomáš Glawaty <tomasglawaty@icloud.com>
2025-06-09 06:28:58 +0200

    Fixed Slack message length and format issues

--- src/Presenter/HomepagePresenter.php
+++ src/Presenter/HomepagePresenter.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace App\Presenter;
 
 use App\Application\ApiHandler\ApiHandlerInterface;
+use Nette\Application\Responses\JsonResponse;
 use Nette\Application\UI\Presenter as NettePresenter;
 use Nette\Http\IRequest;
+use Throwable;
+use Tracy\Debugger;
 
 final class HomepagePresenter extends NettePresenter
 {
@@ -18,11 +21,25 @@ final class HomepagePresenter extends NettePresenter
 
     public function actionDefault(): void
     {
-        if ($this->getHttpRequest()->isMethod(IRequest::Post)) {
-            $response = $this->apiHandler->handle(
-                request: $this->getHttpRequest(),
-                response: $this->getHttpResponse(),
-            );
+        $httpRequest = $this->getHttpRequest();
+
+        if ($httpRequest->isMethod(IRequest::Post)) {
+            $httpResponse = $this->getHttpResponse();
+
+            try {
+                $response = $this->apiHandler->handle(
+                    request: $httpRequest,
+                    response: $httpResponse,
+                );
+            } catch (Throwable $e) {
+                Debugger::log($e, Debugger::ERROR);
+                $httpResponse->setCode($httpResponse::S500_InternalServerError);
+
+                $response = new JsonResponse([
+                    'accepted' => false,
+                    'error' => 'Internal server error.',
+                ]);
+            }
 
             $this->sendResponse($response);
         }
